99762626d53de35e5376e5b95daf011074304585
lucasw0908 <141011290+lucasw0908@users.noreply.github.com>
2024-07-13 04:32:43 +0800

    bug fixed: * log file is read only

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,10 +1,11 @@
 import logging
 import logging.handlers
+import os
 from flask import Flask
 from flask.logging import default_handler
 from flask_wtf import CSRFProtect
 
-from .config import DevConfig, ProdConfig
+from .config import DevConfig, ProdConfig, BASEDIR
 from .models import db
 
 
@@ -27,7 +28,7 @@ def init_logger(debug: bool=False) -> None:
         log.setLevel(logging.INFO)
         
     file_handler = logging.handlers.RotatingFileHandler(
-        filename=f"app.log",
+        filename=os.path.join(BASEDIR, "app.log"),
         encoding="utf-8",
         maxBytes=8**7, 
         backupCount=8
